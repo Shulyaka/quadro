19515a1e42a6d4d8559d23fcaf0d9dacfadb13ae
Denis Shulyaka <Shulyaka@gmail.com>
2013-08-20 00:39:25 +0400

    some more work on accel calibration method

--- newaccelcalibrate.ino
+++ newaccelcalibrate.ino
@@ -82,8 +82,8 @@ bool lsolve(fixed a[9][9], fixed r[9], fixed x[9])  //solves a linear system usi
   byte i, j, counter=0;
   
   for(i=0; i<n; i++)   //initial values:
-    x2[i]=x[i];        //assuming that the provided ones are close enough to the real
-  //  x2[i]=0;           //assuming that the provided ones are dummy
+  //  x2[i]=x[i];        //assuming that the provided ones are close enough to the real
+    x2[i]=0;           //assuming that the provided ones are dummy
   
   printmatrix("a", a);
   print9row("r", r);
@@ -392,7 +392,7 @@ bool lsolve_float(fixed a[9][9], fixed r[9], fixed x[9])
 bool lcheck(float a[9][9], float r[9], float x[9])
 {
   const byte n=9;
-  float delta=0.00001;
+  float delta=0.0001;
   float s=0, e;
   for(byte i=0; i<n; i++)
   {
@@ -402,9 +402,9 @@ bool lcheck(float a[9][9], float r[9], float x[9])
     s=s+e*e;
   }
   
-  if(s>delta)
+  if(abs(s)>delta)
   {
-    print("lcheck failed", s);
+    print("lcheck  failed", abs(s));
     return false;
   }
 //  print("lcheck", s);
@@ -485,16 +485,17 @@ bool lsolve(float a[9][9], float r[9], float x[9])  //solves a linear system usi
 {
   const byte n=9;
   float x2[n];
-  const float eps=0.00001;
+  const float eps=0.000000001;
   float norm=1.0;
-  byte i, j, counter=0;
+  byte i, j;
+  int counter=4096;
   
   for(i=0; i<n; i++)   //initial values:
   //  x2[i]=x[i];        //assuming that the provided ones are close enough to the real
     x2[i]=0;           //assuming that the provided ones are dummy
   
-//  printmatrix("a", a);
-//  print9row("r", r);
+  printmatrix("a", a);
+  print9row("r", r);
 
 //  rearrange(a, r);
   
@@ -505,7 +506,7 @@ bool lsolve(float a[9][9], float r[9], float x[9])  //solves a linear system usi
 
   while(norm>=eps)
   {
-//    print9row("lsolve x", x2);
+    print9row("lsolve x", x2);
     
     for(i=0; i<n; i++)
     {
@@ -526,17 +527,76 @@ bool lsolve(float a[9][9], float r[9], float x[9])  //solves a linear system usi
       x[i]=x2[i];
     }
     
-    if(++counter==0)
+    if(--counter==0)
     {
-      error("Unable to solve the system");
-      return false;
+      error("Unable to solve the system! Falling back to det");
+      return lsolve_det(a,r,x);
     }
   }
   
-//  print9row("solved! x", x);
+  print9row("solved! x", x);
 
-  return lcheck(a, r, x);
+  return true;//lcheck(a, r, x);
 }
+
+bool lsolve2(fixed a[9][9], fixed r[9], fixed x[9])  //solves a linear system using iterational Gauss-Seidel method;  Guaranteed only for diagonal-dominant or symmetric positive-definite matrix
+{
+  const byte n=9;
+  fixed x2[n];
+  const fixed eps=10;
+  fixed norm=one;
+  byte i, j;
+  int counter=4096;
+  
+  for(i=0; i<n; i++)   //initial values:
+  //  x2[i]=x[i];        //assuming that the provided ones are close enough to the real
+    x2[i]=0;           //assuming that the provided ones are dummy
+  
+  printmatrix("a", a);
+  print9row("r", r);
+
+//  rearrange(a, r);
+  
+//  print("det", det(a));
+  
+//  printmatrix("system rearranged", a);
+//  print9row("r", r);
+
+  while(norm>=eps)
+  {
+    print9row("lsolve x", x2);
+    
+    for(i=0; i<n; i++)
+    {
+      norm=r[i];
+      for(j=0; j<i; j++)
+        norm=norm-a[i][j]*x2[j];
+      for(j=i+1; j<n; j++)
+        norm=norm-a[i][j]*x[j];
+      x2[i]=norm%one/a[i][i];
+    }
+    
+    norm=abs(x[0]-x2[0]);
+    x[0]=x2[0];
+    for(i=1; i<n; i++)
+    {
+      if(abs(x[i]-x2[i])>norm)
+        norm=abs(x[i]-x2[i]);
+      x[i]=x2[i];
+    }
+    
+    if(--counter==0)
+    {
+      error("Unable to solve the system! Falling back to float");
+      return lsolve_float(a,r,x);
+    }
+  }
+  
+  print9row("solved! x", x);
+
+  return true;//lcheck(a, r, x);
+}
+
 
 bool lsolve_det(float a[9][9], float r[9], float x[9])
 {
@@ -566,7 +626,9 @@ bool lsolve_det(float a[9][9], float r[9], float x[9])
       for(i=0; i<n; i++)
         m[i][j]=a[i][j];
   }
-
+  
+  print9row("solved_det! x", x);
+  
   return lcheck(a, r, x);
 }
 
@@ -604,7 +666,7 @@ float pow(float a, byte i)
     case 3:
       return a*a*a;
     case 4:
-      return sq(a*a);
+      return (a*a)*(a*a);
     default:
       error("Unsupported power");
   }
@@ -612,9 +674,9 @@ float pow(float a, byte i)
   return 0;
 }
 
-bool math_magic(fixed p[10][3], fixed x[9])          //finds the best calibration parameters using the least squires mathod and iterational Newton's method
+bool math_magic(fixed p[20][3], fixed x[9])          //finds the best calibration parameters using the least squires mathod and iterational Newton's method
 {
-  byte n=10;
+  byte n=20;
   fixed x2[9];
   fixed r[9];
   fixed a[9][9];
@@ -644,8 +706,7 @@ bool math_magic(fixed p[10][3], fixed x[9])          //finds the best calibratio
     {
       r[j]=0;
       for(i=0; i<n; i++)
-        r[j]=r[j] - p2[i][j/3] * pow(p[i][j/3], j%3) * f[i];     //-J'*f/4
-      r[j]=r[j]>>1;
+        r[j]=r[j] - ((p2[i][j/3] * pow(p[i][j/3], j%3) * f[i]));//>>3);     //-J'*f/16
     }
 
     for(i=0; i<9; i++)
@@ -653,30 +714,30 @@ bool math_magic(fixed p[10][3], fixed x[9])          //finds the best calibratio
       {
         a[i][j]=0;
         for(k=0; k<n; k++)
-          a[i][j]=a[i][j] + p2[k][i/3] * pow(p[k][i/3], i%3) * p2[k][j/3] * pow(p[k][j/3], j%3);    //J'*J/4
+          a[i][j]=a[i][j] + ((p2[k][i/3] * pow(p[k][i/3], i%3) * p2[k][j/3] * pow(p[k][j/3], j%3))>>1);    //J'*J/16
       }
 
-//    for(k=0; k<n; k++)                                           //Comment this out to get Gauss-Newton method
-//      for(i=0; i<3; i++)
-//        for(j=0; j<3; j++)
-//          for(l=0; l<9; l+=3)
-//            a[i+l][j+l]=a[i+l][j+l]+( (f[k] * pow(p[k][l/3], i+j)) >> 1);    //f(i)*H(i)/4, i=0..n
+    for(k=0; k<n; k++)                                           //Comment this out to get Gauss-Newton method
+      for(i=0; i<3; i++)
+        for(j=0; j<3; j++)
+          for(l=0; l<9; l+=3)
+            a[i+l][j+l]=a[i+l][j+l]+( (f[k] * pow(p[k][l/3], i+j)) >> 2);    //f(i)*H(i)/16, i=0..n
 
     disable_sensor_interrupts(); //for speed
-    if(!lsolve_float(a, r, x2))
+    if(!lsolve2(a, r, x2))
     {
       enable_sensor_interrupts();
       return false;
     }
     enable_sensor_interrupts();
     
-    norm=abs(x[0]-x2[0]);
-    x[0]=x2[0];
+    norm=abs(x2[0]);
+    x[0]=x[0]+(x2[0]>>2);
     for(i=1; i<n; i++)
     {
-      if(abs(x[i]-x2[i])>norm)
-        norm=abs(x[i]-x2[i]);
-      x[i]=x2[i];
+      if(abs(x2[i])>norm)
+        norm=abs(x2[i]);
+      x[i]=x[i]+(x2[i]>>2);
     }
     
     if(++counter==0)
@@ -691,9 +752,9 @@ bool math_magic(fixed p[10][3], fixed x[9])          //finds the best calibratio
   return true;
 }
 
-bool math_magic(float p[10][3], float x[9])          //finds the best calibration parameters using the least squires mathod and iterational Newton's method
+bool math_magic(float p[20][3], float x[9])          //finds the best calibration parameters using the least squires mathod and iterational Newton's method
 {
-  byte n=10;
+  byte n=20;
   float x2[9];
   float r[9];
   float a[9][9];
@@ -706,6 +767,8 @@ bool math_magic(float p[10][3], float x[9])          //finds the best calibratio
   for (i=0; i<9; i++)  //initial values:
   //  x2[i]=x[i];        //assuming that the provided ones are close enough to the real
     x2[i]=0;           //assuming that the provided ones are dummy
+  
+  //x[1]=0.25;
   
   while(norm>=eps)
   {
@@ -723,8 +786,7 @@ bool math_magic(float p[10][3], float x[9])          //finds the best calibratio
     {
       r[j]=0;
       for(i=0; i<n; i++)
-        r[j]=r[j] - p2[i][j/3] * pow(p[i][j/3], j%3) * f[i];     //-J'*f/16
-      r[j]=r[j]/8.0;
+        r[j]=r[j] - ((p2[i][j/3] * pow(p[i][j/3], j%3) * f[i]) / 8.0);     //-J'*f/16
     }
 
     for(i=0; i<9; i++)
@@ -732,8 +794,7 @@ bool math_magic(float p[10][3], float x[9])          //finds the best calibratio
       {
         a[i][j]=0;
         for(k=0; k<n; k++)
-          a[i][j]=a[i][j] + p2[k][i/3] * pow(p[k][i/3], i%3) * p2[k][j/3] * pow(p[k][j/3], j%3);    //J'*J/16
-        a[i][j]=a[i][j]/4.0;
+          a[i][j]=a[i][j] + ((p2[k][i/3] * pow(p[k][i/3], i%3) * p2[k][j/3] * pow(p[k][j/3], j%3)) / 4.0);    //J'*J/16
       }
 
     for(k=0; k<n; k++)                                           //Comment this out to get Gauss-Newton method
@@ -771,16 +832,17 @@ bool math_magic(float p[10][3], float x[9])          //finds the best calibratio
   return true;
 }
 
-bool math_magic_float(fixed p[10][3], fixed x[9])
+bool math_magic_float(fixed p[20][3], fixed x[9])
 {
   const byte n=9;
-  float p2[10][3];
+  const byte m=20;
+  float p2[m][3];
   float x2[n];
   
   for(byte i=0; i<n; i++)
     x2[i]=(x[i]==one)?1.0:(float)(x[i].value)/2147483648.0;
   
-  for(byte i=0; i<10; i++)
+  for(byte i=0; i<m; i++)
     for(byte j=0; j<3; j++)
       p2[i][j]=(p[i][j]==one)?1.0:(float)(p[i][j].value)/2147483648.0;
   
@@ -808,8 +870,8 @@ bool math_magic_float(fixed p[10][3], fixed x[9])
 
 bool accel_calibrate_manual_3() //manual accel calibration
 {
-  const byte n=10;
-  fixed point[10][3];
+  const byte n=20;
+  fixed point[n][3];
   fixed k[9];
   
 //  Serial.println("Manual accel calibration\nThis algorithm will estimate accel zero values\nby measuring gravity in 6 different positions.\nThe more the positions differ, the better estimation.\nThe positions are not required to be exactly aligned to gravity in any way.\nPlease do not move your quadro.");
@@ -855,6 +917,45 @@ point[9][1]=-0x2D413CCD;
 point[9][2]=0;
 
 
+point[10][0]=0x2839FD28;
+point[10][1]=-0x2D413CCD;
+point[10][2]=0;
+
+point[11][0]=-0x2839FD28;
+point[11][1]=0x2D413CCD;
+point[11][2]=0;
+
+point[12][0]=-0x2839FD28;
+point[12][1]=0;
+point[12][2]=0x2D413CCD;
+
+point[13][0]=0x2839FD28;
+point[13][1]=0;
+point[13][2]=0x2D413CCD;
+
+point[14][0]=-0x2839FD28;
+point[14][1]=0;
+point[14][2]=-0x2D413CCD;
+
+point[15][0]=0;
+point[15][1]=-0x2D413CCD;
+point[15][2]=-0x2D413CCD;
+
+point[16][0]=0;
+point[16][1]=0x2D413CCD;
+point[16][2]=-0x2D413CCD;
+
+point[17][0]=0;
+point[17][1]=-0x2D413CCD;
+point[17][2]=0x2D413CCD;
+
+point[18][0]=0x20D845D1;
+point[18][1]=0x24F34E8B;
+point[18][2]=0x24F34E8B;
+
+point[19][0]=-0x20D845D1;
+point[19][1]=-0x24F34E8B;
+point[19][2]=-0x24F34E8B;
 
 
   for(byte i=0; i<3; i++)
@@ -888,7 +989,7 @@ point[9][2]=0;
     print("sqrt", sqrt(point[i][0]%point[i][0]+point[i][1]%point[i][1]+point[i][2]%point[i][2]));
   }
 
-  if(!math_magic_float(point, k)) // find gain to be applied for the n points to be on a sphere
+  if(!math_magic(point, k)) // find gain to be applied for the n points to be on a sphere
     {error("Cannot find calibration parameters. Possible reasons: programmer was drunk. Please try again.");
     delay(5000);
     return false;
